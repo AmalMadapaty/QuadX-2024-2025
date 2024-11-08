51b3d20e20fd0cd906d98bea612c7b9da2b8123c
AlexIsHere7 <106027707+AlexIsHere7@users.noreply.github.com>
2024-11-08 09:53:02 -0600

    11/9/24 HuskyLens Alignment Code(Experimental)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HuskyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HuskyTeleOp.java
@@ -5,36 +5,36 @@ import android.renderscript.Sampler;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+@TeleOp
 public class HuskyTeleOp extends OpMode {
-    /*
     private DcMotor fl;
     private DcMotor fr;
     private DcMotor bl;
     private DcMotor br;
     private Servo IntakeServoR;
-
-     */
     HuskyLens Husk;
+
 
     @Override
     public void init() {
-        /*
         fl = hardwareMap.dcMotor.get("fl");
         fr = hardwareMap.dcMotor.get("fr");
         bl = hardwareMap.dcMotor.get("bl");
         br = hardwareMap.dcMotor.get("br");
         IntakeServoR = hardwareMap.servo.get("IntakeR");
 
-         */
+
         Husk = hardwareMap.get(HuskyLens.class,"Husky");
-/*
+
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
+         Husk.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
 
         fl.setDirection(DcMotor.Direction.FORWARD);
         fr.setDirection(DcMotor.Direction.REVERSE);
@@ -51,20 +51,35 @@ public class HuskyTeleOp extends OpMode {
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
- */
     }
 
+    //This checks if there is an object. If there is, return how many.
+    //Mind you, this only checks for BLUE objects. TODO: DOES NOT WORK ON RED OR YELLOW YET.
     public int BlueObjectDetection(){
-        Husk.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
         HuskyLens.Block[] List = Husk.blocks();
        return(List.length);
     };
 
+    //This returns the X position of a detected object (only blue at the moment).
+    //For usage of the values, use Posi.
+    public int BlueObjectOrientation() {
+        Husk.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
+        int Xpos = 0;
+        for (HuskyLens.Block b : Husk.blocks()) {
+            if (b.id == 1) {
+                Xpos = b.x;
+                break;
+            }
+        }
+        return Xpos;
+    }
+
     int Object;
+    float Posi;
 
     @Override
     public void loop() {
-        /*
+
         float drive = gamepad1.left_stick_y;
         float turn = -gamepad1.right_stick_x;
         float strafe = gamepad1.left_stick_x;
@@ -104,10 +119,6 @@ public class HuskyTeleOp extends OpMode {
             IntakeServoR.setPosition(0.5);
         }
 
-
-         */
-
-
            Object = BlueObjectDetection();
 
             if (Object >= 1){
@@ -118,16 +129,56 @@ public class HuskyTeleOp extends OpMode {
                 telemetry.addData("Vision",0);
             }
 
+            Posi = BlueObjectOrientation() - 160;
+            telemetry.addData("X Position",Posi);
 
+            //TODO: THIS IS EXPERIMENTAL AND NEEDS TO BE TESTED/FIXED.
+            //This function allows for alignment with detected objects. If there aren't any to align with, it attempts to find one.
+            //NOTE - This might override all other Driver 1 functions temporarily. Whether or not this is bad or good is yet to be determined.
+            if(gamepad1.b) {
+                double time;
+                if (Object == 0) {
+                    time = getRuntime();
+                    while (Object == 0) {
+                        //This makes the robot rotate in an attempt to find an object.
+                        //Should make at least a full 360 before forcefully stopping.
+                        flPower = 0.2;
+                        frPower = -0.2;
+                        blPower = 0.2;
+                        brPower = -0.2;
+
+                        //Failsafe - this prevents infinite spinning from not detecting an object.
+                        //Probably will be referred to as the "Spin Cycle"
+                        if (getRuntime() >= time + 2000){
+                            break;
+                        }
+                    }
+
+                }
+
+                if (Object == 1) {
+                    //This sets the turn variable for the drive functions to an aligning and adaptive value.
+                    //The 0.05 is effectively a derivative - it sets a minimum SOR (Speed Of Rotation).
+                    //The division by 200 can be changed to be lower/higher.
+                    //Higher division will result in a overall slower SOR, and vice versa for Lower division.
+                    //May need to set a range of appropriate values.
+                    //TODO: Check if the turns are proper. They may be skewed by other values. Delete this after.
+                    while (!(Posi == 160)) {
+                        flPower = Range.clip(drive + ((Posi / 200) + 0.05) - strafe, -1.0, 1.0);
+                        frPower = Range.clip(drive - ((Posi / 200) - 0.05) + strafe, -1.0, 1.0);
+                        blPower = Range.clip(drive + ((Posi / 200) - 0.05) + strafe, -1.0, 1.0);
+                        brPower = Range.clip(drive - ((Posi / 200) + 0.05) - strafe, -1.0, 1.0);
+                    }
+                }
+            }
 
         updateTelemetry(telemetry);
-/*
-        fl.setPower(0.1);
-        fr.setPower(0.1);
-        bl.setPower(0.1);
-        br.setPower(0.1);
 
- */
+        fl.setPower(flPower);
+        fr.setPower(frPower);
+        bl.setPower(blPower);
+        br.setPower(brPower);
+
 
     }
 }
